f32748bdb057e44c4b814224b8dd58f6046196c2
Walter Scott <w@lter.ca>
2024-05-28 21:19:00 -0500

    project set up

---
+++ Sources/SpatialAWS/SpatialAWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  spatial_awsApp.swift
+//  spatial-aws
+//
+//  Created by walter on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SpatialAWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
